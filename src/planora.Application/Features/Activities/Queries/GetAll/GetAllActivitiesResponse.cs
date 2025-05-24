f299f5c6ac549248d824de780e3d3ffdaf0bc727
Lucas Gomes <lucasggomees@gmail.com>
2025-05-25 00:46:58 +0100

    feat: enhance GetAllActivities response and add mapping logic

--- src/planora.Application/Features/Activities/Queries/GetAll/GetAllActivitiesResponse.cs
+++ src/planora.Application/Features/Activities/Queries/GetAll/GetAllActivitiesResponse.cs
@@ -3,4 +3,12 @@ namespace planora.Application.Features.Activities.Queries.GetAll;
 public sealed record GetAllActivitiesResponse
 {
     public Guid Id { get; init; }
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+    public DateTimeOffset ActivityDate { get; init; }
+    public required string Category { get; init; }
+    public bool IsActive { get; init; }
+    public required string City { get; init; }
+    public required string Location { get; init; }
+    public string? Coordinates { get; init; }
 }
