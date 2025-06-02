673a5c57ca6a166b4bcd18808a8e22bfa28142bf
Lucas Gomes <lucasggomees@gmail.com>
2025-06-02 23:08:45 +0100

    refactor: update ApiController and ActivitiesController to use a protected IMediator

--- src/planora.API/Common/ApiController.cs
+++ src/planora.API/Common/ApiController.cs
@@ -5,7 +5,11 @@ namespace planora.API.Common;
 
 [Route("api/[controller]")]
 [ApiController]
-public abstract class ApiController(IMediator mediator) : ControllerBase
+public abstract class ApiController : ControllerBase
 {
-    protected readonly IMediator Mediator = mediator;
+    private IMediator? _mediator;
+
+    protected IMediator Mediator => _mediator ??=
+        HttpContext.RequestServices.GetService<IMediator>() ??
+        throw new InvalidOperationException("Mediator is null");
 }
